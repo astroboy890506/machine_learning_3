15178cdbe7a1246e2e80f086aacefa6285525c33
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2023-08-24 15:44:46 +0800

    Update name.py

--- name.py
+++ name.py
@@ -1,19 +1,22 @@
 import streamlit as st
 import nltk
 
+names = [(name, 'male') for name in nltk.corpus.names.words("male.txt")]
+names += [(name, 'female') for name in nltk.corpus.names.words("female.txt")]
+
 def extract_gender_features(name):  #extract the feature
     name = name.lower()
     features = {}
-#     features["suffix"] = name[-1:]
-#     features["suffix2"] = name[-2:] if len(name) > 1 else name[0]
-#     features["suffix3"] = name[-3:] if len(name) > 2 else name[0]
-#     features["suffix4"] = name[-4:] if len(name) > 3 else name[0]
-#     features["suffix5"] = name[-5:] if len(name) > 4 else name[0]
-#     features["suffix6"] = name[-6:] if len(name) > 5 else name[0]
-#     features["prefix"] = name[:1]
-#     features["prefix2"] = name[:2] if len(name) > 1 else name[0]
-#     features["prefix3"] = name[:3] if len(name) > 2 else name[0]
-#     features["prefix4"] = name[:4] if len(name) > 3 else name[0]
+    features["suffix"] = name[-1:]
+    features["suffix2"] = name[-2:] if len(name) > 1 else name[0]
+    features["suffix3"] = name[-3:] if len(name) > 2 else name[0]
+    features["suffix4"] = name[-4:] if len(name) > 3 else name[0]
+    features["suffix5"] = name[-5:] if len(name) > 4 else name[0]
+    features["suffix6"] = name[-6:] if len(name) > 5 else name[0]
+    features["prefix"] = name[:1]
+    features["prefix2"] = name[:2] if len(name) > 1 else name[0]
+    features["prefix3"] = name[:3] if len(name) > 2 else name[0]
+    features["prefix4"] = name[:4] if len(name) > 3 else name[0]
     features["prefix5"] = name[:5] if len(name) > 4 else name[0]
     #features["wordLen"] = len(name)
     #print (features)
